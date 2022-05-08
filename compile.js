50a6bcacb70881cd24a6e2d0ad071785d63c41f9
elmirnascimento <elmirsnascimento@gmail.com>
2022-05-08 16:01:25 -0300

    npm init and installation of solc

--- compile.js
+++ compile.js
@@ -1,5 +1,15 @@
+const appContractsDir = 'src/org/esndev/inbox/contracts';
+const appContractsInit = 'Inbox.sol';
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'src/org/esndev/inbox/contracts','Inbox.sol')
+const appPath = path.resolve(__dirname, appContractsDir,appContractsInit);
+const source = fs.readFileSync(appPath, 'utf8')
+
+
+module.exports = solc.compile(source, 1);
+console.log(solc.compile(source, 1));
+console.log("COMPILED " + appContractsInit + " SUSCESSFULLY." +
+    " \nNEXT WARNINGS SHOULD BE IGNORED.");
+
